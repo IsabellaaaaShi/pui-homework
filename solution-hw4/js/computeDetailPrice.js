c029305948961ee4dfa7b72ef2f9e4aee1428e0f
IsabellaaaaShi <157088411+IsabellaaaaShi@users.noreply.github.com>
2024-02-17 22:12:44 -0500

    hw4 setup

---
+++ solution-hw4/js/computeDetailPrice.js
@@ -0,0 +1,64 @@
+// define the basic case------------------------------------
+const basePrice = 2.49;
+let currGlazingPrice = 0;
+let currPackSize = 1;
+
+// glazing price -------------------------------------------
+const glazingPrice = {
+    "Keep original": 0,
+    "Sugar milk": 0,
+    "Vanilla milk": 0.5,
+    "Double chocolate": 1.5
+}
+
+const glazingSelect = document.querySelector("select#glazingOption");
+// create dropdown menu for glazing
+// javascript dropdown menu: https://github.com/cmu-spuds/pui-materials/tree/main/in-lab-examples/puinote-lab04/select-example
+// javascript for loop for object: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+for (const [glazing, price] of Object.entries(glazingPrice)) {
+    const option = document.createElement('option');
+    option.text = glazing;
+    option.value = price;
+    glazingSelect.add(option);
+}
+
+function glazingChange(element) {
+    // change the value to a float: https://www.freecodecamp.org/news/how-to-convert-a-string-to-a-number-in-javascript/
+    currGlazingPrice = parseFloat(element.value);
+    //console.log(typeof currGlazingPrice)
+    priceUpdate();
+}
+
+// pack size ------------------------------------------------
+const packSize = {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10
+}
+
+const packSelect = document.querySelector("select#packSizeOption");
+
+// create dropdown menu for price
+// javascript dropdown menu: https://github.com/cmu-spuds/pui-materials/tree/main/in-lab-examples/puinote-lab04/select-example
+// javascript for loop for object: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+for (const [pack, price] of Object.entries(packSize)) {
+    const option = document.createElement('option');
+    option.text = pack;
+    option.value = price;
+    packSelect.add(option);
+}
+
+function packSizeChange(element) {
+    currPackSize = parseFloat(element.value);
+    priceUpdate(); 
+}
+
+// update the price ------------------------------------------
+function priceUpdate() {
+    const priceTotal = (basePrice + currGlazingPrice) * currPackSize; 
+    const priceDisplay = document.querySelector("#finalPrice"); 
+    // round the float: https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+    // update the webpage: lab example
+    priceDisplay.textContent= "$" + priceTotal.toFixed(2); 
+}
